cec9153ef2be9a3d06a9d6e347ba24570e0516ee
alamybs <namakukingkong@gmail.com>
2016-07-02 04:37:48 +0700

    Add rack Cors.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,20 @@ module PokemonApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        if Rails.env.test? || Rails.env.development?
+          origins "*"
+        else
+          origins "*"
+          #origins 'waveofweekend.com', 'www.waveofweekend.com'
+        end
+        resource '*',
+                 :headers     => :any,
+                 :methods     => [:get, :post, :delete, :put, :patch],
+                 :credentials => true,
+                 :max_age     => 0
+      end
+    end
   end
 end
