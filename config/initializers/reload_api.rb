9f994ed9e8928cc2fd583136c15586b05dc14e3a
alamybs <namakukingkong@gmail.com>
2018-05-02 03:45:32 +0700

    Reloading api grape.

--- config/initializers/reload_api.rb
+++ config/initializers/reload_api.rb
@@ -1,7 +1,7 @@
 if Rails.env.development?
-  ActiveSupport::Dependencies.explicitly_unloadable_constants << 'Twitter::API'
+  ActiveSupport::Dependencies.explicitly_unloadable_constants << 'API::Init'
 
-  api_files = Dir[Rails.root.join('app', 'api', '**', '*.rb')]
+  api_files = Dir[Rails.root.join('app','controllers', 'api', '**', '*.rb')]
   api_reloader = ActiveSupport::FileUpdateChecker.new(api_files) do
     Rails.application.reload_routes!
   end
