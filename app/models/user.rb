20b2deee32e80397271668d24af9b6473d6a64ba
Yunan Helmy <helmy@extrainteger.com>
2018-12-24 17:02:31 +0700

    tricky way

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User
   include Her::Model
+
+  def self.base_class
+    self
+  end
+
+  def self.name
+    "User"
+  end
+
+  def _read_attribute attr
+    self.send attr
+  end
+
+  def self.polymorphic_name
+    "voter"
+  end
+
 end
