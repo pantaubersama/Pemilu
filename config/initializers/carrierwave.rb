0f33bf152f820068440174002955b30d5afa4f71
alamybs <namakukingkong@gmail.com>
2018-12-26 19:51:32 +0700

    Fix carierwave config.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
         provider:              "AWS",
         aws_access_key_id:     ENV['AWS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+        aws_secret_access_key: ENV['AWS_KEY_SECRET'],
         region:                "ap-southeast-1"
     }
     config.fog_directory   = ENV['AWS_BUCKET_NAME']
