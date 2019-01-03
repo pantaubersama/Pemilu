e501fbc0464b00f2c31754cbb3d4ed25d7482538
alamybs <namakukingkong@gmail.com>
2019-01-03 10:41:59 +0700

    Add creator entity to janji politik.

--- app/controllers/api/v1/linimasa/janji_politiks/entities/janji_politik.rb
+++ app/controllers/api/v1/linimasa/janji_politiks/entities/janji_politik.rb
@@ -4,5 +4,5 @@ class API::V1::Linimasa::JanjiPolitiks::Entities::JanjiPolitik < Grape::Entity
   expose :body
   expose :created_at
   expose :image
-  expose :user, using: API::V1::Users::Entities::User
+  expose :user, as: :creator, using: API::V1::Users::Entities::User
 end
