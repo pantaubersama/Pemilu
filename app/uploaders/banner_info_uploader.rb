987fcda73bd5c7d42001519cce4e93c2409cade0
Yunan Helmy <helmy@extrainteger.com>
2019-01-04 15:28:57 +0700

    salah uploader

--- app/uploaders/banner_info_uploader.rb
+++ app/uploaders/banner_info_uploader.rb
@@ -4,8 +4,7 @@ class BannerInfoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file if Rails.env.development? || Rails.env.test?
-  storage :fog if !Rails.env.development? && !Rails.env.test?
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
