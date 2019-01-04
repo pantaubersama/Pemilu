9ec9c3ea72d87a702bf220c58eac7e67d425cb6e
Yunan Helmy <helmy@extrainteger.com>
2019-01-04 11:42:40 +0700

    add spec

--- spec/requests/api/v1/pendidikan_politik/questions/questions_spec.rb
+++ spec/requests/api/v1/pendidikan_politik/questions/questions_spec.rb
@@ -118,5 +118,31 @@ RSpec.describe "Api::V1::PendidikanPolitik::Resources::Question", type: :request
       expect(json_response[:data][:questions].size).to eq(0)
     end
   end
+
+  describe "[GET] Endpoint /" do
+    before do
+      FactoryBot.create :question, body: "Lorem gembel"
+      FactoryBot.create :question, body: "Lorem gembel wedus"
+      Question.reindex
+    end
+
+    it "search" do
+      get "/pendidikan_politik/v1/questions?q=gembel"
+      expect(response.status).to eq(200)
+      expect(json_response[:data][:questions].size).to be >= 2
+    end
+
+    it "search not found" do
+      get "/pendidikan_politik/v1/questions?q=telek%20gembel"
+      expect(response.status).to eq(200)
+      expect(json_response[:data][:questions].size).to eq(0)
+    end
+
+    it "search using or" do
+      get "/pendidikan_politik/v1/questions?q=telek%20gembel&o=or"
+      expect(response.status).to eq(200)
+      expect(json_response[:data][:questions].size).to be >= 2
+    end
+  end
   
 end
