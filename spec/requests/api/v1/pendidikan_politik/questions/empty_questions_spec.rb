a4aed53f6d140f78136f971882f4018c1d72fbda
Yunan Helmy <helmy@extrainteger.com>
2019-01-19 15:30:12 +0700

    add reindex

--- spec/requests/api/v1/pendidikan_politik/questions/empty_questions_spec.rb
+++ spec/requests/api/v1/pendidikan_politik/questions/empty_questions_spec.rb
@@ -7,9 +7,11 @@ RSpec.describe "Api::V1::PendidikanPolitik::Resources::Question", type: :request
 
   describe "empty record" do
     it "has no error" do
+      Question.reindex
       get "/pendidikan_politik/v1/questions", headers: stub_auth_headers(@access_token),
         params: {page: 1, per_page: 5}
       expect(response.status).to eq(200)
+      expect(json_response[:data][:questions].size).to eq(0)
     end
   end
 end
--- spec/requests/api/v1/pendidikan_politik/quizzes/empty_quizzes_spec.rb
+++ spec/requests/api/v1/pendidikan_politik/quizzes/empty_quizzes_spec.rb
@@ -7,9 +7,11 @@ RSpec.describe "Api::V1::PendidikanPolitik::Resources::Quizzes", type: :request 
 
   describe "empty record" do
     it "has no error" do
+      Quiz.reindex
       get "/pendidikan_politik/v1/quizzes", headers: stub_auth_headers(@access_token),
         params: {page: 1, per_page: 5}
       expect(response.status).to eq(200)
+      expect(json_response[:data][:quizzes].size).to eq(0)
     end
   end
 end
