579b1b09392c0e8770cefd30055241df1ce810c1
Yunan Helmy <helmy@extrainteger.com>
2019-01-21 00:39:59 +0700

    reindex async + subscriber

--- app/models/janji_politik.rb
+++ app/models/janji_politik.rb
@@ -1,6 +1,7 @@
 class JanjiPolitik < ApplicationRecord
 
-  searchkick text_middle: [:all_fields]
+  searchkick  callbacks: :async, 
+              text_middle: [:all_fields]
   acts_as_paranoid
   has_paper_trail
   mount_uploader :image, AssetPictureUploader
@@ -23,16 +24,16 @@ class JanjiPolitik < ApplicationRecord
     end
     resluts.merge({
                     user:       {
-                      email:     self.user.email,
-                      username:  self.user.username,
-                      verified:  self.user.verified,
-                      id:        self.user.id,
-                      avatar:    self.user.avatar,
-                      full_name: self.user.full_name,
-                      about:     self.user.about,
+                      email:     self.user.try(:email),
+                      username:  self.user.try(:username),
+                      verified:  self.user.try(:verified),
+                      id:        self.user.try(:id),
+                      avatar:    self.user.try(:avatar),
+                      full_name: self.user.try(:full_name),
+                      about:     self.user.try(:about),
                       cluster:   cluster,
                     },
-                    all_fields: ["--", self.title, self.body, self.user.username, self.user.full_name, "--"].compact.join(' ')
+                    all_fields: ["--", self.title, self.body, self.user.try(:username), self.user.try(:full_name), "--"].compact.join(' ')
                   })
   end
 
