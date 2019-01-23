c0e056c8aa08e558a75869482e70ea3d577174d7
Yunan Helmy <helmy@extrainteger.com>
2019-01-23 14:58:43 +0700

    question orang lain

--- spec/requests/api/v1/pendidikan_politik/questions/users_questions_spec.rb
+++ spec/requests/api/v1/pendidikan_politik/questions/users_questions_spec.rb
@@ -17,6 +17,13 @@ RSpec.describe "Api::V1::Users::Resources::Questions", type: :request do
       get "/pendidikan_politik/v1/users/c9242c5a-805b-4ef5-b3a7-2a7f25785cc8/questions"
       expect(json_response[:data][:questions].size).to eq(7)
     end
+
+    it "Have 2 question, optional current_user" do
+      FactoryBot.create :question, user_id: "1036fd3c-04ed-4949-b57c-b7dc8ff3e737"
+      FactoryBot.create :question, question_folder_id: FactoryBot.create(:question_folder).id, user_id: "1036fd3c-04ed-4949-b57c-b7dc8ff3e737"
+      get "/pendidikan_politik/v1/users/1036fd3c-04ed-4949-b57c-b7dc8ff3e737/questions", headers: stub_auth_headers(@access_token)
+      expect(json_response[:data][:questions].size).to eq(2)
+    end
   end
 
 end
