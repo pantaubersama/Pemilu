7f75559553936fc32474e04dba01bbed67320387
alamybs <namakukingkong@gmail.com>
2019-01-28 10:17:59 +0700

    Fix params in publisher notif.

--- app/services/publishers/quiz_notification.rb
+++ app/services/publishers/quiz_notification.rb
@@ -6,7 +6,7 @@ module Publishers
       #  - exchange: "pemilu.quiz"
       #  - message:
       #         - {quiz_id: UUID, notif_type: :quiz, event_type: :quiz_created }
-      push exchange, message, :notification
+      push exchange, message
     end
   end
 end
