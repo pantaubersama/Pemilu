7ff1ddc2f729ca2d8426be6efb260a0ef9e272a8
alamybs <namakukingkong@gmail.com>
2019-02-07 15:08:28 +0700

    fix broadcast notif when question upvote.

--- app/services/publishers/question_notification.rb
+++ app/services/publishers/question_notification.rb
@@ -6,7 +6,7 @@ module Publishers
       #  - exchange: "pemilu.question"
       #  - message:
       #         - {question_id: UUID, count: 100, notif_type: :question, event_type: :upvote_report }
-      #         - {question_id: UUID, receiver_id: UUID, user_action_id: UUID, notif_type: :question, event_type: :upvote_report }
+      #         - {question_id: UUID, receiver_id: UUID, user_action_id: UUID, notif_type: :question, event_type: :upvote }
       push exchange, message
     end
   end
