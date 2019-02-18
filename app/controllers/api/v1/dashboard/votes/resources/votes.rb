5940a8f6e18970e6a79bcf700fb21acc033a1955
alamybs <namakukingkong@gmail.com>
2019-02-18 18:19:29 +0700

    Allow `surveymanual` to multiple vote by admin dashboard.

---
+++ app/controllers/api/v1/dashboard/votes/resources/votes.rb
@@ -0,0 +1,44 @@
+module API::V1::Dashboard::Votes::Resources
+  class Votes < API::V1::ApplicationResource
+    helpers API::V1::Helpers do
+      params :votable! do
+        requires :id, type: String, desc: 'Votable ID'
+        requires :class_name, type: String, values: %w[Question ViolationReport::Report], desc: 'Votable class name'
+      end
+    end
+    before do
+      authorize_admin!
+    end
+    resource "votes" do
+      desc "Vote something" do
+        headers AUTHORIZATION_HEADERS
+        detail "Vote something"
+      end
+      params do
+        use :votable!
+        requires :vote_count, type: Integer, desc: "Upvote count"
+      end
+      oauth2
+      post "/" do
+        authorize_voter!
+        question_must_not_in_folder!
+        unixtimestamp = Time.zone.now
+        if params.vote_count < 1
+          params.vote_count = 1
+        end
+        [*1..params.vote_count].map { |index| votable.liked_by current_user, :vote_scope => "voter_#{unixtimestamp}_#{index}" }
+        present :vote, { status: votable.vote_registered? }, with: API::V1::Votes::Entities::VoteRegistered
+      end
+    end
+
+    helpers do
+      def votable
+        @votable ||= ::Kernel.const_get(params.class_name).find(params.id)
+      end
+
+      def question_must_not_in_folder!
+        error!('Pertanyaan sudah terpilih', 404) if votable.is_a?(Question) && votable.question_folder_id.present?
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v1/dashboard/votes/votes_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe API::V1::Dashboard::Votes::Resources::Votes do
+  describe 'POST /dashboard/v1/votes' do
+    it 'likes question' do
+      expect { like question }.to change { question.reload.cached_votes_up }.by(10)
+      expect(response).to have_http_status(:created)
+      expect(json_response).to match(data: a_hash_including(vote: a_hash_including(status: true)))
+    end
+
+    it 'can like liked question by the same person' do
+      question.liked_by user
+      expect { like question }.to change { question.reload.cached_votes_up }
+      expect(response).to have_http_status(:created)
+      expect(json_response).to match(data: a_hash_including(vote: a_hash_including(status: true)))
+    end
+
+    it 'cannot like foldered question' do
+      like foldered_question
+      expect(response).to have_http_status(:not_found)
+    end
+
+    def like(question)
+      params = { id: question.id, class_name: 'Question', vote_count: 10 }
+      user.update(username: :surveymanual, is_admin: true)
+      post '/dashboard/v1/votes', headers: stub_surveymanual_auth_headers(user: user), params: params
+    end
+  end
+
+
+  let(:user) { create :user }
+  let(:question) { create :question, user_id: user.id }
+  let(:foldered_question) { create :foldered_question, user_id: user.id }
+end
