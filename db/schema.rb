f69e40fb4a05e9dbd524b519572b151e7e2d34e6
myusuf <muhammadyusuf931@gmail.com>
2019-02-26 16:02:04 +0700

    add model candidate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_082854) do
+ActiveRecord::Schema.define(version: 2019_02_26_083619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 2019_02_26_082854) do
     t.string "header_image"
     t.string "image"
     t.string "page_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "candidates", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "gender"
+    t.integer "political_party_id"
+    t.integer "electoral_district_id"
+    t.integer "serial_number"
+    t.string "original_filename"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
