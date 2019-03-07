5d673d7ae8ca82b8a041197aa2dbe2e23ce25b78
myusuf <muhammadyusuf931@gmail.com>
2019-03-07 10:01:15 +0700

    fix list candidates by dapil

--- app/controllers/api/v1/hitung/candidates/resources/candidates.rb
+++ app/controllers/api/v1/hitung/candidates/resources/candidates.rb
@@ -10,18 +10,75 @@ module API::V1::Hitung::Candidates::Resources
         requires :dapil_id, type: Integer, desc: "Dapil ID"
         requires :tingkat, type: String, values: %w[dpr provinsi kabupaten dpd], desc: "Tingkat Pemilihan"
       end
-      paginate per_page: Pagy::VARS[:items], max_per_page: Pagy::VARS[:max_per_page]
-
       get "/" do
-        candidates = Candidate.joins(:dapil)
+        query_candidates = Candidate.joins(:dapil)
           .where(electoral_district_id: params.dapil_id)
           .where("dapils.tingkat = ? ", Dapil.tingkats[params.tingkat])
-          .order(political_party_id: :asc)
           .order(serial_number: :asc)
 
-        resources = paginate(candidates)
-        present :candidates, resources, with: API::V1::Hitung::Candidates::Entities::Candidate
-        present_metas resources
+        if params.tingkat == "dpd"
+          candidates = query_candidates
+          candidates_group = candidates.group_by { |candidate| candidate.dapil }.map { |dapil, candidate|
+            {
+              id: dapil.id,
+              nama: dapil.nama,
+              actor_type: "Dapil",
+              tingkat: dapil.tingkat,
+              candidates: candidate.map { |c|
+                {
+                  id: c.id,
+                  name: c.name,
+                  gender: c.gender,
+                  serial_number: c.serial_number,
+                  actor_type: "Candidate",
+                  dapil: {
+                    id: c.dapil.id,
+                    nama: c.dapil.nama,
+                    tingkat: c.dapil.tingkat,
+                  }
+                }
+              }
+            }
+          }
+        else
+          candidates = query_candidates
+            .order(political_party_id: :asc)
+            .joins(:political_party)
+            .select("candidates.name as name, candidates.id as id, candidates.gender as gender, candidates.electoral_district_id as electoral_district_id, candidates.serial_number as serial_number,  political_parties.id as political_party_id")
+
+          candidates_group = candidates.group_by { |party| party.political_party }.map { |party, candidate|
+            {
+              id: party.id,
+              serial_number: party.serial_number,
+              name: party.name,
+              acronym: party.acronym,
+              logo: party.logo,
+              actor_type: "Partai",
+              candidates: candidate.map { |c|
+                {
+                  id: c.id,
+                  name: c.name,
+                  gender: c.gender,
+                  serial_number: c.serial_number,
+                  actor_type: "Candidate",
+                  dapil: {
+                    id: c.dapil.id,
+                    nama: c.dapil.nama,
+                    tingkat: c.dapil.tingkat,
+                  },
+                  political_party: {
+                    id: c.political_party.id,
+                    serial_number: c.political_party.serial_number,
+                    name: c.political_party.name,
+                    acronym: c.political_party.acronym,
+                    logo: c.political_party.logo,
+                  },
+                }
+              },
+            }
+          }
+        end
+        present candidates_group
       end
     end
   end
--- spec/requests/api/v1/hitung/candidates/candidate_spec.rb
+++ spec/requests/api/v1/hitung/candidates/candidate_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe "API::V1::Hitung::Candidates", type: :request do
     it "should return 200" do
       get "/hitung/v1/candidates", params: {
         dapil_id: 1,
-        political_party_id: 1,
         tingkat: "dpr"
       }
       expect(response.status).to eq(200)
@@ -15,71 +14,57 @@ RSpec.describe "API::V1::Hitung::Candidates", type: :request do
     describe "DPD" do
       it "should return 200 and list DPD Caleg" do
         dapil = FactoryBot.create(:dapil, tingkat: 3)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
+        populate_candidate dapil
         get "/hitung/v1/candidates", params: {
           dapil_id: dapil.id,
           tingkat: "dpd"
         }
         expect(response.status).to eq(200)
-        expect(json_response[:data][:candidates].size).to eq(2)
       end
     end
 
     describe "DPR" do
       it "should return 200 and list DPR Caleg by Political Party" do
         dapil = FactoryBot.create(:dapil, tingkat: 0)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
+        populate_candidate dapil
         get "/hitung/v1/candidates", params: {
           dapil_id: dapil.id,
           tingkat: "dpr"
         }
         expect(response.status).to eq(200)
-        expect(json_response[:data][:candidates].size).to eq(3)
-        expect(json_response[:data][:candidates][0][:electoral_district_id]).to eq(dapil.id)
-        expect(json_response[:data][:candidates][1][:electoral_district_id]).to eq(dapil.id)
-        expect(json_response[:data][:candidates][2][:electoral_district_id]).to eq(dapil.id)
       end
     end
 
     describe "Provinsi" do
       it "should return 200 and list Provinsi Caleg by Political Party" do
         dapil = FactoryBot.create(:dapil, tingkat: 1)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
+        populate_candidate dapil
         get "/hitung/v1/candidates", params: {
           dapil_id: dapil.id,
           tingkat: "provinsi"
         }
         expect(response.status).to eq(200)
-        expect(json_response[:data][:candidates].size).to eq(3)
-        expect(json_response[:data][:candidates][0][:electoral_district_id]).to eq(dapil.id)
-        expect(json_response[:data][:candidates][1][:electoral_district_id]).to eq(dapil.id)
-        expect(json_response[:data][:candidates][2][:electoral_district_id]).to eq(dapil.id)
       end
     end
 
     describe "Kabupaten" do
       it "should return 200 and list Kabupaten Caleg by Political Party" do
         dapil = FactoryBot.create(:dapil, tingkat: 2)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
-        FactoryBot.create(:candidate, electoral_district_id: dapil.id)
+        populate_candidate dapil
         get "/hitung/v1/candidates", params: {
           dapil_id: dapil.id,
           tingkat: "kabupaten"
         }
         expect(response.status).to eq(200)
-        expect(json_response[:data][:candidates].size).to eq(3)
-        expect(json_response[:data][:candidates][0][:electoral_district_id]).to eq(dapil.id)
-        expect(json_response[:data][:candidates][1][:electoral_district_id]).to eq(dapil.id)
-        expect(json_response[:data][:candidates][2][:electoral_district_id]).to eq(dapil.id)
       end
     end
 
-
+    def populate_candidate dapil
+      FactoryBot.create(:dapil)
+      FactoryBot.create(:political_party)
+      FactoryBot.create(:candidate, electoral_district_id: dapil.id)
+      FactoryBot.create(:candidate, electoral_district_id: dapil.id)
+      FactoryBot.create(:candidate, electoral_district_id: dapil.id)
+    end
   end
 end
