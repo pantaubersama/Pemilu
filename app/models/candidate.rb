75aa7db7f97444055d7f467a510b8b22baafcebe
myusuf <muhammadyusuf931@gmail.com>
2019-03-07 10:01:15 +0700

    fix list candidates by dapil

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,3 +1,4 @@
 class Candidate < ApplicationRecord
   belongs_to :dapil, foreign_key: :electoral_district_id
+  belongs_to :political_party, foreign_key: :political_party_id, optional: true
 end
--- app/models/political_party.rb
+++ app/models/political_party.rb
@@ -1,2 +1,3 @@
 class PoliticalParty < ApplicationRecord
+  has_many :candidates, foreign_key: :political_party_id
 end
