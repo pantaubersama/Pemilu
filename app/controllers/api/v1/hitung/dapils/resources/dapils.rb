72be24f3234d619b5b6b6db2c368308d35b68eac
Yunan Helmy <m.yunan.helmy@gmail.com>
2019-03-08 12:00:42 +0700

    refactor get dapil to model

--- app/controllers/api/v1/hitung/dapils/resources/dapils.rb
+++ app/controllers/api/v1/hitung/dapils/resources/dapils.rb
@@ -11,34 +11,16 @@ module API::V1::Hitung::Dapils::Resources
         requires :tingkat, type: String, values: %w[dpr provinsi kabupaten dpd], desc: "Tingkat Pemilihan"
       end
       get "/" do
-        case params.tingkat
-        when "dpd"
-          province = Province.find_by(code: params.province_code)
-          error!("Record Province tidak ditemukan", 422) unless province
+        province = Province.find_by(code: params.province_code)
+        error!("Record Province tidak ditemukan", 422) unless province
 
-          dapil = Dapil.where(tingkat: 3, idWilayah: province.id_wilayah).first
-        when "dpr"
-          regency = Regency.find_by(code: params.regency_code)
-          error!("Record Regency tidak ditemukan", 422) unless regency
+        regency = Regency.find_by(code: params.regency_code)
+        error!("Record Regency tidak ditemukan", 422) unless regency
 
-          dapil = DapilWilayah.joins(:dapil)
-            .where(idWilayah: regency.id_wilayah)
-            .where("dapils.tingkat = ?", 0).last.dapil
-        when "provinsi"
-          regency = Regency.find_by(code: params.regency_code)
-          error!("Record Regency tidak ditemukan", 422) unless regency
+        district = District.find_by(code: params.district_code)
+        error!("Record District tidak ditemukan", 422) unless district
 
-          dapil = DapilWilayah.joins(:dapil)
-            .where(idWilayah: regency.id_wilayah)
-            .where("dapils.tingkat = ?", 1).last.dapil
-        when "kabupaten"
-          district = District.find_by(code: params.district_code)
-          error!("Record District tidak ditemukan", 422) unless district
-
-          dapil = DapilWilayah.joins(:dapil)
-            .where(idWilayah: district.id_wilayah)
-            .where("dapils.tingkat = ?", 2).last.dapil
-        end
+        dapil = Dapil.by_wilayah params.tingkat, province, regency, district
         present dapil, with: API::V1::Hitung::Dapils::Entities::Dapil
       end
     end
