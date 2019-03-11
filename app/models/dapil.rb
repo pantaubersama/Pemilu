5eb99f416d146e7e251b8119736a07102459a981
myusuf <muhammadyusuf931@gmail.com>
2019-03-11 23:55:31 +0700

    update dapils

--- app/models/dapil.rb
+++ app/models/dapil.rb
@@ -22,4 +22,19 @@ class Dapil < ApplicationRecord
     end
     dapil
   end
+
+  def self.dapils_by_region level, province=nil, regency=nil
+    case level
+    when "dpd"
+      dapils = Dapil.where(tingkat: 3).order("nama asc")
+    when "dpr"
+      dapils = Dapil.where(tingkat: 0, idWilayah: province.id_wilayah).order("id asc")
+    when "provinsi"
+      dapils = Dapil.where(tingkat: 1, idWilayah: province.id_wilayah).order("id asc")
+    when "kabupaten"
+      dapils = Dapil.where(tingkat: 2, idWilayah: regency.id_wilayah).order("id asc")
+    end
+    dapils
+  end
+
 end
